d3fd6bf087392a27db7c5514508b688467257c50
artur-moraes <100148375+Artur-Moraes@users.noreply.github.com>
2023-12-16 18:32:51 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,14 +4,14 @@ distancias = [((pos[0] - posicoes[0][0])**2 + (pos[1] - posicoes[0][1])**2)**0.5
 tempoTolerancia = [int(distancia * 1.5) for distancia in distancias]
 
 for i, distancia in enumerate(distancias):
-    print(f"Distância do cliente {i + 1} para a sede: {distancia} unidades")
-    print(f"Tempo de tolerância do cliente {i + 1}: {tempoTolerancia[i]} minutos")
+    print(f"Distância do cliente {i + 1} para a sede: {round(distancia)} unidades")
+    print(f"Tempo de tolerância do cliente {i + 1}: {round(tempoTolerancia[i])} minutos")
 
 pedido = [[1, 4], [3, 3], [2, 1]]
-informacaoPedido = [{'cliente': p[0], 'quantidade_agua': p[1]} for p in pedido]
+informacaoPedido = [{'cliente': p[0], 'quantidade de água': p[1]} for p in pedido]
 
 for info in informacaoPedido:
-    print(f"Cliente {info['cliente']} pediu {info['quantidade_agua']} galões de água.")
+    print(f"Cliente {info['cliente']} pediu {info['quantidade de água']} galões de água.")
 
 """
 
